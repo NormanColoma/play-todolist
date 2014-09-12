04a432099f55006fe3bb805a28cf895293788357
Norman Coloma <ua.norman@gmail.com>
2014-09-12 22:46:43 +0200

    Añadida redirección a lista de tareas

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("App MADS UA"))
+    Redirect(routes.Application.tasks)
   }
 
   def tasks = TODO

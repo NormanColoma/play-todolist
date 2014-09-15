31c936a914aef399dd32e90a501b1b0a3fc3f0d9
Norman Coloma <ua.norman@gmail.com>
2014-09-15 19:28:46 +0200

    Creada base de datos
    -Definida base de datos
    -Creadas las queries SQL

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,4 +1,8 @@
 package models
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
 
 case class Task(id: Long, label: String)
 
@@ -9,5 +13,15 @@ object Task {
   def create(label: String) {}
   
   def delete(id: Long) {}
-  
+
+  def all(): List[Task] = DB.withConnection { implicit c =>
+  	SQL("select * from task").as(task *)
+  }
+
+  val task = {
+	  get[Long]("id") ~ 
+	  get[String]("label") map {
+	    case id~label => Task(id, label)
+	  }
+  }	
 }

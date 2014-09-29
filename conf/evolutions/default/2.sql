61179516ac47f4133d3e3ff911ac6107d061f650
Norman Coloma <ua.norman@gmail.com>
2014-09-29 11:39:41 +0200

    Añadido archivo 2.sql
    -Creada tabla task_user
    -Modificada tabla task: agregada foreign key

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,19 @@
+ # --- !Ups
+
+CREATE TABLE task_user (
+    name varchar(50) NOT NULL, 
+    PRIMARY KEY(name)
+);
+
+ALTER TABLE task ADD t_user varchar(50);
+ALTER TABLE task ADD CONSTRAINT fk_task_tuser FOREIGN KEY(t_user) REFERENCES task_user(name);
+
+INSERT INTO task_user (name) values('anonimo');
+INSERT INTO task_user (name) values('domingogallardo');
+INSERT INTO task_user (name) values('norman');
+ 
+# --- !Downs
+
+DELETE FROM task_user;
+ALTER TABLE task DROP t_user;
+DROP TABLE user_task;

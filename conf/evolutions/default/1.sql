d645dc4b8a68965a87fd2b586aca02a9f5dcb7e2
Norman Coloma <ua.norman@gmail.com>
2014-10-13 21:11:54 +0200

    Modificando evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -7,8 +7,18 @@ CREATE TABLE task (
     id integer NOT NULL DEFAULT nextval('task_id_seq'),
     label varchar(255)
 );
+
+CREATE TABLE task_user (
+    name varchar(50) NOT NULL, 
+    PRIMARY KEY(name)
+);
+
+ALTER TABLE task ADD t_user varchar(50) DEFAULT 'anonimo';
+ALTER TABLE task ADD CONSTRAINT fk_task_tuser FOREIGN KEY(t_user) REFERENCES task_user(name);
  
 # --- !Downs
  
-DROP TABLE task;
+ALTER TABLE task DROP t_user;
+DROP TABLE  IF EXISTS task;
+DROP TABLE  IF EXISTS task_user;
 DROP SEQUENCE task_id_seq;
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,13 +1,5 @@
  # --- !Ups
 
-CREATE TABLE task_user (
-    name varchar(50) NOT NULL, 
-    PRIMARY KEY(name)
-);
-
-ALTER TABLE task ADD t_user varchar(50) DEFAULT 'anonimo';
-ALTER TABLE task ADD CONSTRAINT fk_task_tuser FOREIGN KEY(t_user) REFERENCES task_user(name);
-
 INSERT INTO task_user (name) values('anonimo');
 INSERT INTO task_user (name) values('domingogallardo');
 INSERT INTO task_user (name) values('norman');
@@ -16,6 +8,4 @@ INSERT INTO task_user (name) values('norman');
 
 DELETE FROM task;
 DELETE FROM task_user;
-ALTER TABLE task DROP t_user;
-DROP TABLE task;
-DROP TABLE task_user;
+

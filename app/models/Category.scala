f99577c04728f0151abc9a7e3d182627c552dd37
Norman Coloma <ua.norman@gmail.com>
2014-11-12 18:39:05 +0100

    Creado test de modelo para feature4

---
+++ app/models/Category.scala
@@ -0,0 +1,18 @@
+package models
+import play.api.db._
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+import java.util.Date
+
+case class Category(id: Long, name: String, t_user:String)
+
+
+
+object Category {
+	val category = {
+      get[Long]("id") ~ 
+      get[String]("label") ~
+      get[String]("t_user")
+   }
+}
---
+++ test/TestModel_feature4.scala
@@ -0,0 +1,26 @@
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+import models._
+
+class ModelTaskF4 extends Specification {
+   
+  "Task model_feature4" should {
+	    "creating categories for users" in {
+		    running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+		    	val cat1= Category.newCategory("Sports","norman")
+		    	val cat2= Category.newCategory("Videos","norman")
+		    	cat1.id muestEqual 1
+		    	cat1.name must equalTo("Sports")
+		    	cat1.t_user must equalTo("norman")
+		    	cat2.id mustEqual 2
+		    	cat2.name must equalTo("Videos")
+		    	cat2.t_user must equalTo("norman")
+		    }
+		}
+	}
+}
